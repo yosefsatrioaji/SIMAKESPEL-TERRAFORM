fae3a48b313d5cce80294ff4eba75675e25d4646
Yosef Satrio Aji <yosef260501@gmail.com>
2023-02-18 01:10:16 +0700

    ready?

---
+++ backend/firewall/main.tf
@@ -0,0 +1,35 @@
+resource "google_compute_firewall" "firewalli-int" {
+  name    = "${terraform.workspace}-firewall-int"
+  network = "${var.vpc_name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  source_ranges = ["${var.ip_cidr_range}"]
+}
+
+# Create a firewall rule that allows external SSH, ICMP, and HTTPS:
+resource "google_compute_firewall" "firewalli-ext" {
+  name    = "${terraform.workspace}-firewall-ext"
+  network = "${var.vpc_name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "6443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
--- platform/main.tf
+++ platform/main.tf
@@ -1,7 +1,31 @@
-module "config" {
-  source = "../modules/config"
+module "vpc" {
+  source = "../backend/vpc"
 }
 
-data "google_project" "project" {
-  project_id = local.project_id
+module "subnet" {
+  source      = "../backend/subnet"
+  region      = "${var.region}"
+  vpc_name     = "${module.vpc.vpc_name}"
+  subnet_cidr = "${var.subnet_cidr}"
+}
+
+module "firewall" {
+  source        = "../backend/firewall"
+  vpc_name       = "${module.vpc.vpc_name}"
+  ip_cidr_range = "${module.subnet.ip_cidr_range}"
+}
+
+module "cloudsql" {
+  source                     = "../cloudsql"
+  region                     = "${var.region}"
+  availability_type          = "${var.availability_type}"
+  sql_instance_size          = "${var.sql_instance_size}"
+  sql_disk_type              = "${var.sql_disk_type}"
+  sql_disk_size              = "${var.sql_disk_size}"
+  sql_require_ssl            = "${var.sql_require_ssl}"
+  sql_master_zone            = "${var.sql_master_zone}"
+  sql_connect_retry_interval = "${var.sql_connect_retry_interval}"
+  sql_replica_zone           = "${var.sql_replica_zone}"
+  sql_user                   = "${var.sql_user}"
+  sql_pass                   = "${var.sql_pass}"
 }
